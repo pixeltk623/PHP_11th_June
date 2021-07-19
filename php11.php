0d86a7293921f0817d71487e0ab323a02d3db8e3
sunilkanjariya <sunilrkanjariya@gmail.com>
2021-07-19 11:03:55 +0530

    task

---
+++ php11.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<table border="1" style="border-collapse: collapse;">
+		<tr>
+			<td>Earnings</td>
+			<td>Amount in Rs.</td>
+			<td>Deductions</td>
+			<td>Amount in Rs.</td>
+		</tr>
+		<tr>
+			<td style="bor">BASIC</td>
+			<td>9,965.00</td>
+			<td>PROVIDENT FUND</td>
+			<td>1,196</td>
+		</tr>
+		<tr>
+			<td>HOUSE RENT ALLOWANCE</td>
+			<td>4,982.00</td>
+			<td>PROFESSIONAL TAX</td>
+			<td>200.00</td>
+		</tr>
+		<tr>
+			<td>SPECIAL ALLOWANCE</td>
+			<td>7,093.00</td>
+			<td>INCOME TAX</td>
+			<td>194.00</td>
+		</tr>
+		<tr>
+			<td>CONVEYANCE</td>
+			<td>800.00</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>ADDNL SPECIAL ALLOWANCE</td>
+			<td>3,333.00</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>ONCALL/SHIFT ALLOWANCE</td>
+			<td>1,800.00</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>STAT BONUS</td>
+			<td>700.00</td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>GROSS EARNING</td>
+			<td>28,673.00</td>
+			<td>GROSS DEDUCTIONS</td>
+			<td>1,590.00</td>
+		</tr>
+		<tr>
+			<td colspan="2">NET PAY</td>
+			<td colspan="2">27,083.00</td>
+		</tr>
+	</table>
+</body>
+</html>
