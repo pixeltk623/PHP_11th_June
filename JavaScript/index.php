1338eaa50b52782f96b5d18fc33257b60aa3aba4
shreetuition <info@shreetuitioncenter.com>
2021-09-15 15:33:39 +0530

    Done

---
+++ JavaScript/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+</head>
+<body>
+
+	<h1 id="hello"></h1>
+
+	<h1 id="test">This is Js</h1>
+
+<script type="text/javascript">
+	"use strict"
+
+	//alert
+	//confirm()
+	//prompt
+
+	// var a = parseInt(prompt("Enter The First Number"))
+	// var b = parseInt(prompt("Enter The Second Number"))
+
+	// if (isNaN(a)) {
+	// 	console.log("First Number can Not be blank")
+	// }
+	// if (isNaN(b)) {
+	// 	console.log("Second Number can Not be blank")
+	// }
+
+	// if (!isNaN(a) && !(isNaN(b))) {
+	// 	console.log(a+b);
+	// }
+
+	// if (isNaN(a)) {
+	// 	console.log("Hai")
+	// } else {
+	// 	console.log("Nahi Hai")
+	// }
+
+	
+
+	// if ((a==null || a=='') && (b==null || b=='')) {
+	// 	console.log("Kuch Galat Hai")
+	// } else {
+	// 	console.log(a+b);
+	// }
+
+	// console.log(a);
+
+	// console.log(isNaN(eval(a)))
+
+	// if (a=='') {
+	// 	console.log("Nahi Hai");
+	// }
+
+	// var h1var = document.getElementById('test');
+
+	// var demo = confirm();
+
+	// if (demo) {
+	// 	//alert("Deleted");
+	// 	h1var.style.display = "none";
+	// } else {
+	// 	alert("Not Deleted")
+	// }
+
+	// console.log(demo)
+
+	// var h1f = document.getElementById("hello").innerHTML = "This is First H1";
+
+	// var h1var = document.getElementById('test');
+
+	// document.getElementById("test").style.cssText = "font-size: 100px; background-color: red; color: white;"
+
+	// console.log(h1var.style)
+	// console.log(h1var.style.fontSize = "100px")
+	// console.log(h1var.style.backgroundColor = "red")
+	// console.log(h1var.style.color = "white")
+	// console.log(h1var)
+
+	// document.getElementId('')
+	// document.getElementsByClassName('')
+	// document.getElementsByTagName('')
+
+	// var username = "Sharvan"
+
+	// document.write("<h1>Hello This is "+username+" Kumar</h1>")
+
+	// var a = parseInt("5");
+
+	// var b = eval("6");
+
+	// string to number conversions
+
+	// parseInt() and eval1()
+
+
+	// console.log(typeof(a));
+
+	// console.log(a+b);
+
+	//console.log(a+b)
+
+	// var firstName = "Sharvan";
+
+	// var lastName = "Kumar";
+
+	// console.log(firstName+" "+lastName);
+
+	// let username = "Sharvan"
+
+	// console.log(username)
+
+	// var , let , const
+
+	// alert("This is Kumar")
+
+	// console.log("Hello This is Kumar");
+
+	// for(var i = 1; i<=10; i++) {
+
+	// 	console.log(i)
+	// }
+
+	// console.log(i)
+
+	// for(let i = 1; i<=10; i++) {
+
+	// 	console.log(i)
+	// }
+
+	// console.log(i)
+
+	// const a = 5;
+	// a = 6
+	// alert(a)
+
+</script>
+</body>
+</html>
